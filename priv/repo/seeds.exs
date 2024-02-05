2d262f4d678a2ae9762906c5dbf9791341fb6f22
lubien <lubien1996@gmail.com>
2024-02-05 19:06:46 -0300

    Seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,25 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     BackendFight.Repo.insert!(%BackendFight.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
+  id: 1,
+  limit: 100000,
+  name: "Jonathan"
+})
+BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
+  id: 2,
+  limit: 80000,
+  name: "Joseph"
+})
+BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
+  id: 3,
+  limit: 1000000,
+  name: "Jotaro"
+})
+BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
+  id: 4,
+  limit: 10000000,
+  name: "Josuke"
+})
+BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
+  id: 5,
+  limit: 500000,
+  name: "Giorno"
+})
