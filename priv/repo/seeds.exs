9c94ee12ead2f34eff5b9a0cbfb9a97ef6c0da84
lubien <lubien1996@gmail.com>
2024-02-09 15:50:21 -0300

    Make seeds easier to read

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,25 +1,25 @@
 BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
   id: 1,
-  limit: 100000,
+  limit: 1_000 * 100,
   name: "Jonathan"
 })
 BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
   id: 2,
-  limit: 80000,
+  limit: 800 * 100,
   name: "Joseph"
 })
 BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
   id: 3,
-  limit: 1000000,
+  limit: 10_000 * 100,
   name: "Jotaro"
 })
 BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
   id: 4,
-  limit: 10000000,
+  limit: 100_000 * 100,
   name: "Josuke"
 })
 BackendFight.Repo.insert!(%BackendFight.Bank.Customer{
   id: 5,
-  limit: 500000,
+  limit: 5_000 * 100,
   name: "Giorno"
 })
