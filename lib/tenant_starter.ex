cd2d6998d39a9acd3497ae8e13485c02777d58e0
lubien <lubien1996@gmail.com>
2024-02-17 19:54:09 -0300

    Warm up before validations

--- lib/tenant_starter.ex
+++ lib/tenant_starter.ex
@@ -3,6 +3,7 @@ defmodule TenantStarter do
   This is just a hacky way to create customers bootup for this event.
   We don't need an agent honestly
   """
+  require Logger
   use Agent
 
   def start_link(_opts) do
@@ -18,9 +19,28 @@ defmodule TenantStarter do
     end
 
     TenantSupervisor.create_customer(999, "Warmup", 10_000_000 * 100)
-    for _ <- 1..100_000 do
+    prev = System.monotonic_time()
+    Logger.info("[#{DateTime.utc_now}] Warming up before starting server")
+    Stream.resource(fn -> :ok end, fn _ ->
+        time = System.monotonic_time()
+        seconds = (time - prev)/1_000_000_000
+        if seconds >= 15 do
+          {:halt, :ok}
+        else
+          {[:ok], :ok}
+        end
+      end,
+      fn _ -> :ok end
+    )
+    |> Enum.each(fn _ ->
       SqliteServer.insert_transaction(999, "warmup", "d", 100)
-    end
+    end)
+    Logger.info("[#{DateTime.utc_now}] Warmed up")
+
+    {:ok, _pid} = DynamicSupervisor.start_child(TenantSupervisor, %{
+      id: :endpoint,
+      start: {BackendFightWeb.Endpoint, :start_link, []}
+    })
 
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
