769a16106025b3c12de0c6a75c6a54ff0eabdb44
lubien <lubien1996@gmail.com>
2024-02-17 18:37:28 -0300

    warmup jit

--- lib/tenant_starter.ex
+++ lib/tenant_starter.ex
@@ -17,6 +17,11 @@ defmodule TenantStarter do
       TenantSupervisor.create_customer(index + 1, name, limit)
     end
 
+    TenantSupervisor.create_customer(999, "Warmup", 10_000_000 * 100)
+    for _ <- 1..100_000 do
+      SqliteServer.insert_transaction(999, "warmup", "d", 100)
+    end
+
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 end
